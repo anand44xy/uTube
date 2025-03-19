bc1b4e7d3b5a079dceb57dbaf13555f3c4cae196
Anand Saini <125125041+anand44xy@users.noreply.github.com>
2025-03-19 10:22:23 +0530

    cloudinary completed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,43 +1,27 @@
+// Here we get files from user in local storage and then upload them to cloudinary
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
 
-(async function() {
+(async function () {
 
     // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET 
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY``,
+        api_secret: process.env.CLOUDINARY_API_SECRET
     });
-    
-    // // Upload an image
-    //  const uploadResult = await cloudinary.uploader
-    //    .upload(
-    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-    //            public_id: 'shoes',
-    //        }
-    //    )
-    //    .catch((error) => {
-    //        console.log(error);
-    //    });
-    
-    // console.log(uploadResult);
-    
-    // // Optimize delivery by resizing and applying auto-format and auto-quality
-    // const optimizeUrl = cloudinary.url('shoes', {
-    //     fetch_format: 'auto',
-    //     quality: 'auto'
-    // });
-    
-    // console.log(optimizeUrl);
-    
-    // // Transform the image: auto-crop to square aspect_ratio
-    // const autoCropUrl = cloudinary.url('shoes', {
-    //     crop: 'auto',
-    //     gravity: 'auto',
-    //     width: 500,
-    //     height: 500,
-    // });
-    
-    // console.log(autoCropUrl);    
+
+    const uploadOnCloudinary = async (localFilePath) => {
+        try {
+            if (!localFilePath) return null
+            // Upload the file on cloudinary
+            const uploadedFile = await cloudinary.uploader.upload(localFilePath, { resource_type: 'auto' });
+
+            console.log('File has been uploaded successfully!', uploadedFile.url);
+            return uploadedFile
+        } catch (error) {
+            // Remove the locally saved temporary file  after being uploaded on cloudinary
+            fs.unlinkSync(localFilePath);
+        }
+    }
 })();
